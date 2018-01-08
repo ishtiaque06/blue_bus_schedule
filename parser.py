f69c3ee44c2055bc1dc51536f79ff352958ab633
Ahmed Ishtiaque <ishtiaque@DESKTOP-4LR25C3.localdomain>
2018-01-08 13:56:41 -0500

    Added comments to parser.py

--- parser.py
+++ parser.py
@@ -1,13 +1,20 @@
+#This file parses a csv file which has multiple titles without commas followed by
+#multiple lines of comma-separated values and outputs them into separate csv files
+
+#Open the input file as read-only
 initial = open('bluebus_schedules.csv', 'r')
+
+#loops through the input file
 for line in initial:
-    if ',' not in line:
-        filename = line[:-1] + '.csv'
-        new_file = open(filename, 'w')
-        new_file.close()
-    else:
-        new_file = open(filename, 'a')
+
+    if ',' not in line: #If the line doesn't contain a comma, it's a title
+        filename = line[:-1] + '.csv' #the title is used to make the filename.csv
+        new_file = open(filename, 'w') #new file opened to write to
+        new_file.close() #new file is closed
+        
+    else: #Else, the csv line belongs to the title that was found in a previous line
+        new_file = open(filename, 'a') #appends the csv to the new file
         new_file.write(line[:-1] + "\n")
         new_file.close()
-initial.seek(0)
-
+        
 initial.close()
