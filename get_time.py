955481ebc2623eee3aaba6b0402b1e53764f28bd
Kenzie <kkimtai@gmail.com>
2018-01-08 01:12:55 -0500

    Working get_time function % correctly formatted csv

---
+++ get_time.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#import libraries
+import urllib2
+from bs4 import BeautifulSoup
+
+"""
+Function parses through the Blue_Bus website and retrives Bus times in a CSV file
+"""
+def get_time():
+    #specify the url
+    bus_page = 'http://www.brynmawr.edu/transportation/bico.shtml'
+
+    #query website and return the html
+
+    page = urllib2.urlopen(bus_page)
+
+    #parse the html using BeautifulSoup as a string
+    soup = BeautifulSoup(page, 'lxml') 
+    
+    tables= soup.find_all('table') #finds all the tables with bus times
+    
+    f = open('bus_times.csv','w')
+    
+    for table in tables:
+        for row in table.find_all("tr"):
+            row = row.get_text(", ", strip=True) # removes white spaces and tabs between rows in text
+
+            row = row.encode('ascii', 'ignore') # removes symbols in Unicode and keeps strings
+            row =  ("".join(row.split()))  #removes all whitespaces before, after and inbetween strings
+            f.write(row+"\n")
+            
+    f.close()
+get_time()
+
