795fabac136808c5f85ae16ef19db0726785969c
Kenzie <kkimtai@gmail.com>
2018-01-12 23:41:38 -0500

    Changes in all_csv.py - Handles both Sat. day & night time csvs

--- all_csv.py
+++ all_csv.py
@@ -8,12 +8,21 @@ Function retrieves the day's relevant csv file and returns a list of dictionarie
 """
 def relevant_csv():
     csv_list = parser() #makes list of csvs parsed from the BlueBus Website
-
-    today = (datetime.datetime.now()).strftime('%A')  #gives the name of the day   
+    today = (datetime.datetime.now()).strftime('%A')  #gives the name of the day
     
     for csv in csv_list:
         if today in csv:
+            
             relevant_csv = csv
+            
+        #since there are 2 different CSVs for Saturday: Daytime and Nighttime
+        elif today == "Saturday" and ("Daytime" in csv): 
+                relevant_csv = csv
+        
+        elif today == "Saturday" and ("Night" in csv):
+            relevant_csv = csv
+                
+                               
                 
     time_dict_list = get_data(relevant_csv)  #a list of dictionaries with relevant time for the current day
     
@@ -22,5 +31,4 @@ def relevant_csv():
 
 if __name__ == '__main__':
     relevant_csv()
-
 
--- bus_to_BMC.py
+++ bus_to_BMC.py
@@ -15,7 +15,7 @@ def bus_to_BMC():
    
     for time_dict in  time_dict_list:
         time_now =  datetime.datetime.now()  #current time
-        time_in_2hrs = datetime.datetime.now() + datetime.timedelta(hours=1.75) #the point after the next 3 buses
+        time_in_2hrs = datetime.datetime.now() + datetime.timedelta(hours=1.5) #the point after the next 3 buses
         
         bus_time = str(timestring.Date(time_dict['LeaveHaverford'])) #formats to today's time as a string
         bus_time = datetime.datetime.strptime(bus_time, '%Y-%m-%d %H:%M:%S') #converts the time string into a datetime object for comparison
