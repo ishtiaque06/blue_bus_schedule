f1fb8b53a7c82509830d835e121091a07c6e8b0b
Kenzie <kkimtai@gmail.com>
2018-01-29 00:31:34 -0500

    formatted time strings

--- bus_to_HC.py
+++ bus_to_HC.py
@@ -15,14 +15,14 @@ def bus_to_HC():
     next_buses = ""
     for time_dict in  time_dict_list:
         time_now =  datetime.datetime.now()  #current time
-        time_in_2hrs = datetime.datetime.now() + datetime.timedelta(hours=1.5) #the point after the next 3 buses
+        time_in_2hrs = datetime.datetime.now() + datetime.timedelta(hours=2.0) #the point after the next 3 buses
         
         bus_time = str(timestring.Date(time_dict['LeaveBrynMawr'])) #formats to today's time as a string
         
         bus_time = datetime.datetime.strptime(bus_time, '%Y-%m-%d %H:%M:%S') #converts the time string into a datetime object for comparison
         if time_now <= bus_time and time_in_2hrs >= bus_time:
         
-            next_buses += str(time_dict['LeaveBrynMawr']) + ',' + "\n"     
+            next_buses += str(time_dict['LeaveBrynMawr']) + "\n"     
      
     if next_buses != "":
         return next_buses
